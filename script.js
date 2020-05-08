77e038272d06a07e5b5a3bf267dd9995b5c2cac0
Joao Areno <joao.areno2006@gmail.com>
2020-05-07 22:35:18 -0300

    Added undo and redo functions

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 function init(){
+
+    if(localStorage.getItem("text")){
+        document.getElementById("input").innerHTML = localStorage.getItem("text");
+    }
+
+    document.getElementById("input").addEventListener('input', function(){
+        localStorage.setItem('text', document.getElementById("input").innerHTML.toString());
+    });
+
     document.getElementById("b-button").addEventListener('click', function(){changeText("bold")});
-    document.getElementById("i-button").addEventListener('click', function(){changeText("italic")})
+    document.getElementById("i-button").addEventListener('click', function(){changeText("italic")});
+    document.getElementById("undo-button").addEventListener('click', function(){changeText("undo")});
+    document.getElementById("redo-button").addEventListener('click', function(){changeText("redo")});
 }
 
 function changeText(commandToExec){
     document.execCommand(commandToExec);
 }
 
-document.addEventListener('load', init())
+document.addEventListener('load', init());
